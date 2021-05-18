96959d75a526c0fc3d314742e9036d4957ecf3bd
Rupinder Virdi <35664372+rockingvirdi96@users.noreply.github.com>
2021-05-18 15:26:50 -0400

    random

--- client/src/actions/postAction.js
+++ client/src/actions/postAction.js
@@ -1,78 +1,80 @@
-import { GET_MY_POSTS, LIKE_POST, GET_POSTS_FOR_ANY_USER, GET_FRIENDS_POSTS } from "./types";
+import {
+  GET_MY_POSTS,
+  LIKE_POST,
+  GET_POSTS_FOR_ANY_USER,
+  GET_FRIENDS_POSTS,
+} from "./types";
 import axios from "axios";
 
-export const getMyPosts = () => async dispatch => {
+export const getMyPosts = () => async (dispatch) => {
+  try {
+    axios.defaults.headers.common["x-auth-token"] = JSON.parse(
+      sessionStorage.getItem("auth")
+    );
+    const res = await axios.get(" /posts/");
+    const data = res.data.reverse();
+    dispatch({
+      type: GET_MY_POSTS,
+      data: data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    try {
+export const getPostsForAnyUser = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`http://localhost:5000/posts/getpost/${id}`);
+    const data = res.data.reverse();
+    dispatch({
+      type: GET_POSTS_FOR_ANY_USER,
+      data: data,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-        axios.defaults.headers.common['x-auth-token'] = JSON.parse(sessionStorage.getItem('auth'));
-        const res = await axios.get("http://localhost:5000/posts/")
-        const data = res.data.reverse();
-        dispatch(
-            {
-                type: GET_MY_POSTS,
-                data: data
-            }
-        )
-    } catch (error) {
-        console.log(error);
+export const getAllPostsForFriends = () => async (dispatch) => {
+  try {
+    axios.defaults.headers.common["x-auth-token"] = JSON.parse(
+      sessionStorage.getItem("auth")
+    );
+    const res = await axios.get(`http://localhost:5000/posts/friends`);
+    const data = res.data.reverse();
+    dispatch({
+      type: GET_FRIENDS_POSTS,
+      data: data,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-    }
-}
+export const likePost = (id) => async (dispatch) => {
+  try {
+    axios.defaults.headers.common["x-auth-token"] = JSON.parse(
+      sessionStorage.getItem("auth")
+    );
+    const res = await axios.post(`http://localhost:5000/posts/like/${id}`);
+    console.log(res);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-
-export const getPostsForAnyUser = (id) => async dispatch => {
-
-    try {
-        const res = await axios.get(`http://localhost:5000/posts/getpost/${id}`)
-        const data = res.data.reverse();
-        dispatch(
-            {
-                type: GET_POSTS_FOR_ANY_USER,
-                data: data
-            }
-        )
-
-    } catch (error) {
-        console.log(error.message);
-    }
-}
-
-export const getAllPostsForFriends = () => async dispatch => {
-    try {
-
-        axios.defaults.headers.common['x-auth-token'] = JSON.parse(sessionStorage.getItem('auth'));
-        const res = await axios.get(`http://localhost:5000/posts/friends`)
-        const data = res.data.reverse();
-        dispatch(
-            {
-                type: GET_FRIENDS_POSTS,
-                data: data
-            }
-        )
-    } catch (error) {
-        console.log(error.message);
-    }
-}
-
-
-export const likePost = (id) => async dispatch => {
-    try {
-        axios.defaults.headers.common['x-auth-token'] = JSON.parse(sessionStorage.getItem('auth'));
-        const res = await axios.post(`http://localhost:5000/posts/like/${id}`);
-        console.log(res);
-    } catch (error) {
-        console.log(error.message);
-    }
-}
-
-export const addPost = (text, image) => async dispatch => {
-    try {
-        axios.defaults.headers.common['x-auth-token'] = JSON.parse(sessionStorage.getItem('auth'));
-        const res = await axios.post(`http://localhost:5000/posts/`, { text, image });
-        console.log(res);
-        window.location.reload();
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+export const addPost = (text, image) => async (dispatch) => {
+  try {
+    axios.defaults.headers.common["x-auth-token"] = JSON.parse(
+      sessionStorage.getItem("auth")
+    );
+    const res = await axios.post(`http://localhost:5000/posts/`, {
+      text,
+      image,
+    });
+    console.log(res);
+    window.location.reload();
+  } catch (error) {
+    console.log(error.message);
+  }
+};
